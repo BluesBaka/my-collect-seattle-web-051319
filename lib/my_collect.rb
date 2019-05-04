1e2d05345eb655257c87bb24a844cd94cc8f300a
Bryna Dunnells <badunnells@gmail.com>
2019-05-04 19:26:46 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,9 +2,9 @@ def my_collect(array) # put argument(s) here
    i = 0
    new_collection = []
    while i <array.length
-
+      new_collection << yield(array[i])
      i +=1
-     new_collection << yield(array[i])
+     
    end# code here
    new_collection
 end
