f9c8deb5fef99b3858202ed81c5fad01d1c0d21a
Bryna Dunnells <badunnells@gmail.com>
2019-05-04 19:23:10 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,10 @@
 def my_collect(array) # put argument(s) here
    i = 0
+   new_collection = []
    while i <array.length
      yield array[i]
      i = i +1
+     new_collection << i 
    end# code here
    
 end
